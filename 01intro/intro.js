e91b03723f6e70ecff650a56bff3133d85507018
Ferwin Lopez <ferwinlopezt@gmail.com>
2023-10-10 22:36:02 +0530

    first commit

---
+++ 01intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: "Ferwin", age: 10 };
+console.log("Ferwin Lopez");
+console.log(user.age);
---
+++ 01intro/intro.ts
@@ -0,0 +1,3 @@
+let user = {name:"Ferwin", age:10}
+console.log("Ferwin Lopez");
+console.log(user.email);
