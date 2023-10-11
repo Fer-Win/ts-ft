c56c180baddd6f3abc64db4fd05b0bd8f8349551
Ferwin Lopez <ferwinlopezt@gmail.com>
2023-10-11 16:43:26 +0530

    ss/8v

--- 02basics/myfunctions.js
+++ 02basics/myfunctions.js
@@ -1,9 +1,11 @@
 "use strict";
+// const addTwo =(num:number): number=> {
+//   return "num+2"
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-var addTwo = function (num) {
+function addTwo(num) {
     return num + 2;
-    return 'hello'
-};
+}
 function getUpper(val) {
     return val.toUpperCase();
 }
@@ -15,5 +17,24 @@ var loginUser = function (email, name, isPaid) {
 };
 loginUser("hey", "true");
 signUpUser("heythere", '2', false);
-let myValue = addTwo(5);
+addTwo(5);
 getUpper("Hello There");
+function getValue(myVal) {
+    if (myVal > 5) {
+        return true;
+    }
+    return '200 OK';
+}
+var getHello = function (s) {
+    return 'hello there';
+};
+var heros = ['thor', 'spiderman', 'ironman'];
+heros.map(function (hero) {
+    return "hey ".concat(hero);
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
--- 02basics/myfunctions.ts
+++ 02basics/myfunctions.ts
@@ -1,4 +1,8 @@
-const addTwo =(num:number)=> {
+// const addTwo =(num:number): number=> {
+//   return "num+2"
+// }
+
+function addTwo(num:number):number{
   return num+2
 }
 
@@ -19,5 +23,35 @@ signUpUser("heythere",'2',false)
 addTwo(5);
 
 getUpper("Hello There")
+
+
+
+function getValue(myVal:number){
+  if(myVal>5){
+    return true
+  }
+  return '200 OK'
+}
+
+
+const getHello =(s:string):string=>{
+  return 'hello there';
+}
+
+const heros = ['thor','spiderman','ironman'];
+
+
+heros.map( (hero):string=>{
+  return `hey ${hero}`
+})
+
+
+function consoleError(errmsg:string): void{
+  console.log(errmsg);
+
+}
+function handleError(errmsg:string): never{
+throw new Error(errmsg)  
+}
 
 export{}
