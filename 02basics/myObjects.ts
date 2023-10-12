dd01c11f97b0e0249c3f1a11cb17c78597220fb5
Ferwin Lopez <ferwinlopezt@gmail.com>
2023-10-12 11:55:11 +0530

    ss/v10

--- 02basics/myObjects.ts
+++ 02basics/myObjects.ts
@@ -13,4 +13,20 @@ createUser({name:'ferwin',isPaid:false})
 function createCourse():{name: string,price: number}{
   return {name:'rectjs', price:399}
 }
+
+
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean;
+}
+
+function createUsers(user:User): User{
+  return {name:user.name , email:user.email,isActive:user.isActive}
+}
+
+createUsers({name:'',email:'',isActive:true})
+
+
+
 export {}
