24d3b6dca3a21240a60882f4bf1db5049dd7ab33
Ferwin Lopez <ferwinlopezt@gmail.com>
2023-10-12 22:55:24 +0530

    ss/13v

---
+++ 02basics/myTupEnums.ts
@@ -0,0 +1,13 @@
+// const user: (string | number | boolean)[] = ['hc','Ferwin']
+
+
+let tUser: [string, number , boolean];
+
+tUser = ['er',512,true]
+
+
+let rgb : [number,number,number] = [255,123,222];
+
+type Userd = [number,string]
+
+let newUser : Userd = [2,'s']
